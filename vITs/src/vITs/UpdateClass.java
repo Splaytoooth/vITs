6c4200126192a42b0120747bdef592ba25f00df8
matus33 <enjoi_33@yahoo.com>
2015-04-14 14:52:14 +0200

    UpdateÄndringar

--- vITs/src/vITs/UpdateClass.java
+++ vITs/src/vITs/UpdateClass.java
@@ -13,48 +13,57 @@ import javax.swing.JOptionPane;
  * @author Matus
  */
 public class UpdateClass {
-    
+
     public static void main(String[] argv) {
         try {
-                Connection connection = DatabasTest.newConnection();
-                        
-                Statement myStmt = connection.createStatement();
-                
+            Connection connection = DatabasTest.newConnection();
+
+            Statement myStmt = connection.createStatement();
+
                // String sql = "Insert into Reseforskott values ('" + rf.getText() + "','" + rf.getSumma() + "';'" + rf.getValuta() + "')";
-                
-                //myStmt.executeUpdate(sql);
+            //myStmt.executeUpdate(sql);
         } catch (SQLException se) {
             se.printStackTrace();
-        } 
+        }
     }
-    
+
     public static void insertReseforskott(EntityGrej.Reseförskott rf) {
         try {
-                //Connection connection = DriverManager.getConnection("jdbc:mysql://resadb.cnjxqasdqhys.us-west-2.rds.amazonaws.com:3306/resaDB", "resaDB", "resaDB1234");
+            //Connection connection = DriverManager.getConnection("jdbc:mysql://resadb.cnjxqasdqhys.us-west-2.rds.amazonaws.com:3306/resaDB", "resaDB", "resaDB1234");
             Connection connection = DatabasTest.newConnection();
-            
-                Statement myStmt = connection.createStatement();
-                //public Reseförskott(int id, String motivering, int summa, boolean accepterat)
-                String sql = "Insert into Reseförskott(Motivering, Summa, KonsultID, ReseutläggsID, Accepterat) values ('" + rf.getMotivering() + "'," + rf.getSumma() + ", " + rf.getKonsultID() + ", null, null)";
-                
-                myStmt.executeUpdate(sql);
+
+            Statement myStmt = connection.createStatement();
+            //public Reseförskott(int id, String motivering, int summa, boolean accepterat)
+            String sql = "Insert into Reseförskott(Motivering, Summa, KonsultID, ReseutläggsID, Accepterat) values ('" + rf.getMotivering() + "'," + rf.getSumma() + ", " + rf.getKonsultID() + ", null, null)";
+
+            myStmt.executeUpdate(sql);
             JOptionPane.showMessageDialog(null, "Ärende skickat!");
         } catch (SQLException se) {
             JOptionPane.showMessageDialog(null, se);
         }
     }
-    
+
     public static void insertReseutlägg(EntityGrej.Reseutlägg ru, EntityGrej.Utgifter[] utgifter) {
         try {
             Connection conn = DatabasTest.newConnection();
 
             Statement myStmt = conn.createStatement();
+            
+            String sql= "Select TOP 1 0;"
+            
 
-            String sql = "insert into Reseutlägg(StartDatum, SlutDatum, FranLand, TillLand, Accepterat) VALUES('" + ru.getStartDatum() + "', '" + ru.getSlutDatum() + "', '" + ru.getFranLand() + "', '" + ru.getTillLand() + "', null)";
-            
+            String sql = "insert into Reseutlägg(ID, StartDatum, SlutDatum, FranLand, TillLand, Accepterat) VALUES('" + ru.getStartDatum() + "', '" + ru.getSlutDatum() + "', '" + ru.getFranLand() + "', '" + ru.getTillLand() + "', null)";
+
             myStmt.executeUpdate(sql);
-            JOptionPane.showMessageDialog(null, "Ärende skickat!");
+
+            sql = "Select id from Reseutlägg where"
             
+            for each(EntityGrej.Utgifter  in utgifter) {
+                sql = "insert into Utgifter(ReseUtlaggsID, Typ, Summa) VALUES('" + ru.getId() + "'")"
+            }
+
+            JOptionPane.showMessageDialog(null, "Ärende skickat!");
+
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e);
         }
