9f2a5e8a29a8e54f37892f8639c1d54de2c1c4c0
Splaytooth <Splaytooth@hotmail.com>
2015-04-14 13:14:27 +0200

    hej

--- vITs/src/guitest/Ärenden.java
+++ vITs/src/guitest/Ärenden.java
@@ -33,11 +33,11 @@ public class Ärenden extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 944, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 603, Short.MAX_VALUE)
         );
 
         pack();
