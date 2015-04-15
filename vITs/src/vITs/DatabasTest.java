20adf0337b31368b6731e3e0cd62e5b45f60055d
Stafffe <Steffan2f@yahoo.se>
2015-04-15 12:51:36 +0200

    grej

--- vITs/src/vITs/DatabasTest.java
+++ vITs/src/vITs/DatabasTest.java
@@ -13,35 +13,34 @@ import javax.swing.JOptionPane;
  * @author Matus
  */
 public class DatabasTest {
-    
+
     public static void main(String[] argv) {
         try {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://resadb.cnjxqasdqhys.us-west-2.rds.amazonaws.com:3306/resaDB", "resaDB", "resaDB1234");
-        
-        Statement myStmt = connection.createStatement();
-        
-        ResultSet myRs = myStmt.executeQuery("select * from Konsulter");
-        
-        while(myRs.next()) {
-            System.out.println(myRs.getString("Email"));
-                    }
+            Connection connection = DriverManager.getConnection("jdbc:mysql://resadb.cnjxqasdqhys.us-west-2.rds.amazonaws.com:3306/resaDB", "resaDB", "resaDB1234");
+
+            Statement myStmt = connection.createStatement();
+
+            ResultSet myRs = myStmt.executeQuery("select * from Konsulter");
+
+            while (myRs.next()) {
+                System.out.println(myRs.getString("Email"));
+            }
         } catch (SQLException e) {
             System.out.print("Det blev fel");
             e.printStackTrace();
             return;
-        } 
+        }
     }
-    
+
     public static Connection newConnection() {
         try {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://resadb.cnjxqasdqhys.us-west-2.rds.amazonaws.com:3306/resaDB", "resaDB", "resaDB1234");
-        return connection;
+            Connection connection = DriverManager.getConnection("jdbc:mysql://resadb.cnjxqasdqhys.us-west-2.rds.amazonaws.com:3306/resaDB", "resaDB", "resaDB1234");
+            return connection;
         } catch (SQLException e) {
             System.out.print(e);
             JOptionPane.showMessageDialog(null, "Misslyckades att koppla upp till databasen");
             return null;
-        } 
+        }
     }
-    
-    
+
 }
