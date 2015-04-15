765b3c615e2e22a2f8971690c82b3da10357ec3e
Stafffe <Steffan2f@yahoo.se>
2015-04-15 16:43:53 +0200

    Valutor

--- vITs/src/vITs/Valutor.java
+++ vITs/src/vITs/Valutor.java
@@ -3,19 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package vITs;
 
 import java.sql.*;
 import javax.swing.JOptionPane;
-
 
 /**
  *
  * @author 93stealb
  */
 public class Valutor {
-    public static EntityGrej.Valutor HamtaValutor(){
+
+    public static ResultSet HamtaValutor() {
         Connection conn = DatabasTest.newConnection();
+        try {
+            Statement myStmt = conn.createStatement();
+
+            String sql = "Select * from Valutor";
+
+            return myStmt.executeQuery(sql);
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
     }
 }
