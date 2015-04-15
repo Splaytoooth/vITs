69325fbb18c3b8f1dfe71bb5b90156b7d5654972
Stafffe <Steffan2f@yahoo.se>
2015-04-15 12:47:30 +0200

    grej

---
+++ vITs/src/vITs/Valutor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vITs;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author 93stealb
+ */
+public class Valutor {
+    public static EntityGrej.Valutor HamtaValutor(){
+        Connection conn = DatabasTest.newConnection();
+    }
+}
