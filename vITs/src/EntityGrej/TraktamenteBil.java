d72b8b5ab9ba2d5af4fa21e30c1fc47710ed7e35
Stafffe <Steffan2f@yahoo.se>
2015-04-16 13:03:08 +0200

    Traktamente

---
+++ vITs/src/EntityGrej/TraktamenteBil.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EntityGrej;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author 93stealb
+ */
+@Entity
+@Table(name = "TraktamenteBil")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "TraktamenteBil.findAll", query = "SELECT t FROM TraktamenteBil t"),
+    @NamedQuery(name = "TraktamenteBil.findByTyp", query = "SELECT t FROM TraktamenteBil t WHERE t.typ = :typ"),
+    @NamedQuery(name = "TraktamenteBil.findByAvdrag", query = "SELECT t FROM TraktamenteBil t WHERE t.avdrag = :avdrag")})
+public class TraktamenteBil implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "Typ")
+    private String typ;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "Avdrag")
+    private Double avdrag;
+
+    public TraktamenteBil() {
+    }
+
+    public TraktamenteBil(String typ) {
+        this.typ = typ;
+    }
+
+    public String getTyp() {
+        return typ;
+    }
+
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+
+    public Double getAvdrag() {
+        return avdrag;
+    }
+
+    public void setAvdrag(Double avdrag) {
+        this.avdrag = avdrag;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (typ != null ? typ.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof TraktamenteBil)) {
+            return false;
+        }
+        TraktamenteBil other = (TraktamenteBil) object;
+        if ((this.typ == null && other.typ != null) || (this.typ != null && !this.typ.equals(other.typ))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "EntityGrej.TraktamenteBil[ typ=" + typ + " ]";
+    }
+    
+}
