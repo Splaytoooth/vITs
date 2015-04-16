81daad0fc205700af691afeec5375b5000e99acb
Nilsinge <nils_inge87@hotmail.com>
2015-04-16 14:46:34 +0200

    Traktamente

--- vITs/src/localDBbuilder/EmbeddedDBcreator.java
+++ vITs/src/localDBbuilder/EmbeddedDBcreator.java
@@ -6,16 +6,16 @@ import javax.swing.*;
 public class EmbeddedDBcreator {
     
          
-     public static void main(String[] argv) {    
+     public static void main(String[] args) {    
          try {
-             String createString = "CREATE TABLE MORESTUFF3 (STUFFNAME VARCHAR(50) NOT NULL, Stuff_attribute VARCHAR(50))";
+             String createString = "CREATE TABLE MORESTUFF5 (STUFFNAME VARCHAR(50) NOT NULL, Stuff_attribute VARCHAR(50))";
             Connection connection = DriverManager.getConnection("jdbc:derby:myDatabase;create=true");
             Statement myStmt = connection.createStatement();
             System.out.println("Db established");
             myStmt.executeUpdate(createString);
             System.out.println(createString);
             
-            String insertString = "INSERT INTO MORESTUFF3 VALUES ('6', '7') ";
+            String insertString = "INSERT INTO MORESTUFF5 VALUES ('6', '7') ";
             myStmt.executeUpdate(insertString);
             
             
@@ -28,6 +28,40 @@ public class EmbeddedDBcreator {
    
      }
    
+     public static void valutaLandTableCreator ()
+     {
+         try {
+            String createValuta = "CREATE TABLE VALUTA_local (Valuta VARCHAR(50) NOT NULL, Kronor DOUBLE( ))";
+            String createLand   = "CREATE TABLE LAND_local   (Land VARCHAR(50) NOT NULL, Maxbelopp DOUBLE())";
+            
+            Connection connection = DriverManager.getConnection("jdbc:derby:localReseDB;create=true");
+            Statement myStmt = connection.createStatement();
+            System.out.println("Db established");
+            myStmt.executeUpdate(createValuta);
+            myStmt.executeUpdate(createLand);
+ 
+            // Sätter in värden för valuta
+            String insertStringVal1 = "INSERT INTO VALUTA_local VALUES ('EURO', 9.36) ";
+            String insertStringVal2 = "INSERT INTO VALUTA_local VALUES ('USD', 8.79) ";
+            
+            myStmt.executeUpdate(insertStringVal1);
+            myStmt.executeUpdate(insertStringVal2);
+            
+            // Sätter in värden för länder
+            String insertStringLand1 = "INSERT INTO Land_local VALUES ('Sverige', 220) ";
+            String insertStringLand2 = "INSERT INTO Land_local VALUES ('Livland', 278) ";
+            String insertStringLand3 = "INSERT INTO Land_local VALUES ('Livland', 278) ";
+            
+            
+            myStmt.executeUpdate(insertString);
+            
+            } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            System.out.print("fel");
+           
+        }
+         
+     }
     
 
     public static Connection connectDb1() {
