26e8d9cda4dc7390026157571050a62615987a2d
Stafffe <Steffan2f@yahoo.se>
2015-04-21 10:09:54 +0200

    Trakt2

--- vITs/src/EntityGrej/aktUtgifter.java
+++ vITs/src/EntityGrej/aktUtgifter.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -193,6 +194,7 @@ public class aktUtgifter {
                 utg.KostnadInklMoms *= valutaKonv;
             }
 
+            JOptionPane.showMessageDialog(null, this.beraknadeUtgifter);
         }
     }
 }
