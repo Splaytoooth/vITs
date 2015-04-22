7ed5a7e54585732df47801c7a3bcbc49000ebfb9
Nilsinge <nils_inge87@hotmail.com>
2015-04-22 11:31:05 +0200

    Kod för att skapa lokal databas

--- vITs/src/Validering/Valid.java
+++ vITs/src/Validering/Valid.java
@@ -8,12 +8,43 @@ import javax.swing.JTextField;
 
 
 
-
 public class Valid {
     
     
     
+      // metoden kollar så att inmatningsfältet för nytt lösenord inkluderar minst en stor bokstav, en liten bokstav samt en siffra. 
+    static public boolean newPassWordController(JTextField tf) {
+        if (!tf.getText().matches("[0-9]") || !(tf.getText().matches("a-z")) || !(tf.getText().matches("A-Z"))) {  
+            JOptionPane.showMessageDialog(null, "Ett nytt lösenord måste inkludera åtminstone en siffra, en liten bokstav samt en stor bokstav");
+            tf.requestFocus();
+            return true;
+        } else {
+            return false;
+        }
+    }
     
+      // metoden kollar så att inmatningsfältet för nytt lösenord inkluderar minst en stor bokstav, en liten bokstav samt en siffra. 
+    static public boolean newPassWordController2(JTextField tf) {
+        if (!tf.getText().matches("((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])$ )")) {  
+            JOptionPane.showMessageDialog(null, "Ett nytt lösenord måste inkludera åtminstone en siffra, en liten bokstav samt en stor bokstav");
+            tf.requestFocus();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    
+      // metoden kollar så att inmatningsfält ej inkluderar några siffror eller andra symboler utöver bokstäver i det latinska alfabetet 
+    static public boolean onlyText(JTextField tf) {
+        if (!tf.getText().matches("[^0-9]+")) {  // hanterar siffrorna 0-9, bindestreck, samt whitespace, och via + flera återkommande sådana element
+            JOptionPane.showMessageDialog(null, "Fältet stödjer ej siffror ");
+            tf.requestFocus();
+            return true;
+        } else {
+            return false;
+        }
+    }
     
     
     
@@ -37,10 +68,12 @@ public class Valid {
             return true;
         } else {
             return false;
-
         }
     }
 
+   
+    
+    
     // metoden kollar om innehållet i en textarea är tomt
     static public boolean noTextInArea(JTextArea ta) {
         if (ta.getText().length() == 0) {
@@ -52,27 +85,55 @@ public class Valid {
         }
     }
     
-    // kolla så att email-fältet inkl @
+    // kolla så att email-fältet inkl @ och .
     static public boolean emailIncludesAtAndDot(JTextField tf) {
-        if (!(tf.getText().contains("@")) && !(tf.getText().contains("."))) {
+        if (!(tf.getText().contains("@")) || !(tf.getText().contains("."))) {
             JOptionPane.showMessageDialog(null, "Email-fältet måste inkludera @ och .");
             tf.requestFocus();
             return true;
         } else {
 
             return false;
-
         }
-
     }
     
     
-      // metoden är specifik för inmatning av användarnamn
-    // Ett anv.namn får enligt databasen ej bestå utav fler än 6 chars
-    // Vilket denna metod kollar upp
-    static public boolean restrictMotiveringLength (JTextField tf) {
-        if (tf.getText().length() < 20) {
+    /*   Metoden ser till att längden på motiveringen
+    /    ej är för kort 
+    /
+    */
+    static public boolean restrictMotiveringLength (JTextArea ta) {
+        if (ta.getText().length() < 20) {
             JOptionPane.showMessageDialog(null, "Du måste ange en fullständig motivering på minst 20 tecken");
+            ta.requestFocus();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /*
+    /       Kontrollerar så att användaren ej matar in 
+    /       för mycket text i TF
+    */
+     static public boolean restrictTFLength (JTextField tf) {
+        if (tf.getText().length() > 20) {
+            JOptionPane.showMessageDialog(null, "Du får ej mata in fler än 20 tecken i fältet");
+            tf.requestFocus();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    
+   /* Metoden kollar så att användaren ej matar in tomma ytor i textfältet 
+   /  via exempelvis space eller tab
+    / Tillämpningsområden: Förnamn, efternamn, email
+     */
+    static public boolean noSpaces (JTextField tf) {
+        if (!tf.getText().matches("[^-\\s]+") ) {
+            JOptionPane.showMessageDialog(null, "Du får ej mata in tomma ytor i detta fält");
             tf.requestFocus();
             return true;
         } else {
@@ -80,20 +141,23 @@ public class Valid {
         }
     }
     
-    
-    
-   // metoden är specifik för inmatning av användarnamn
-    // Ett anv.namn får enligt databasen ej bestå utav fler än 6 chars
-    // Vilket denna metod kollar upp
-    static public boolean restrictLength (JTextField tf) {
-        if (tf.getText().length() > 10) {
-            JOptionPane.showMessageDialog(null, "Du får ej mata in fler än 10 symboler i detta fält");
+    /*
+    static public boolean startsWithCapLetter (Character checkString) {
+        if (Character.isUpperCase(checkString.charAt(0)) ) {
+            JOptionPane.showMessageDialog(null, "Du får ej mata in tomma ytor i detta fält");
             tf.requestFocus();
             return true;
         } else {
             return false;
         }
     }
+ 
+    */
+    
+    
+    
+    
+    
     
     
 }
