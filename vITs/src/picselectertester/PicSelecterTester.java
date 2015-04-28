286a2498d88f54c7ef3130aed46a6800336a5981
Nilsinge <nils_inge87@hotmail.com>
2015-04-29 01:27:51 +0200

    Lade till testpaket för bildhämtning och storleksjustering av bild.

---
+++ vITs/src/picselectertester/PicSelecterTester.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package picselectertester;
+
+/**
+ *
+ * @author Nils-Inge
+ */
+public class PicSelecterTester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
